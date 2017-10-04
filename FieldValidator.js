ccbca9b785e181f02b9c9c4cd4d2337eb790980f
Robin Jacobs <robin@robinj.be>
2017-10-04 22:22:17 +0100

    Fixed bug in date validation when value is a Date object rather than a String + tests for `before`, `after`, `equal` behaviour and "now"

--- FieldValidator.js
+++ FieldValidator.js
@@ -200,9 +200,13 @@ module.exports = class FieldValidator
 					if (! /^(?:\d{4}\-(?:(?:(?:(?:0[13578]|1[02])\-(?:0[1-9]|[1-2][0-9]|3[01]))|(?:(?:0[469]|11)\-(?:0[1-9]|[1-2][0-9]|30))|(?:02\-(?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)\-02\-29)$/.test (value) && value.constructor.name !== 'Date')
 						return this.invalid (this.name, 'must be a valid date');
 					
+					const date = new Date (value);
+					date.setHours (0, 0, 0, 0);
+					
 					if (parts[1])
 					{
-						let date2 = parts[2] === 'now' ? (new Date ()).setHours (0, 0, 0, 0) : new Date (parts[2]);
+						const date2 = parts[2] === 'now' ? new Date () : new Date (parts[2]);
+						date2.setHours (0, 0, 0, 0);
 						
 						switch (parts[1])
 						{
